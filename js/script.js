a4d404734e34711a47590842324f7788ace4a5e3
Dovlet Atayev <74273489+DendyLA@users.noreply.github.com>
2023-07-08 19:32:05 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
